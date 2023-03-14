1730fcea44ab6675168bf65db6486765f4884598
Santiago Fleitas <sansfleitas@gmail.com>
2023-03-14 11:27:42 -0300

    Use the useReducer hook

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useReducer, useState } from 'react'
 
 interface CreateCycleData {
   task: string
@@ -34,7 +34,21 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cycles, setCycles] = useState<Cycle[]>([])
+  // action vai indicar qual ação vamos fazer para atualizar o estado
+  // método dispatch será o método para disparar essa ação (action), não será mais um método que altera o estado diretamente (setState) e
+  // sim será um método que irá disparar/enviar uma ação
+  // É uma boa prática ao usar dispatch() enviar objetos em vez de informações cruas (raw data)
+  const [cycles, dispatch] = useReducer((state: Cycle[], action: any) => {
+    // o código desta função é executada quando um dispatch() é executado, e o argumento do dispatch irá no lugar do action
+    // Em vez de criar diversos funções handlers para manipular cada tipo de alteração que formos fazer num estado (exemplo: adicionar um elemento à lista, remover, modificar, etc...)
+    // Criamos uma função dentro do useReducer((state, action) => {...}) no qual irá centralizar todos os casos de uso nela, e irá diferenciar as alterações com uso de if's ou switch
+    if (action.type === 'ADD_NEW_CYCLE') {
+      return [...state, action.payload.newCycle]
+    }
+
+    return state
+  }, [])
+
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
@@ -45,15 +59,22 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    setCycles((cycles) =>
-      cycles.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, finishedDate: new Date() }
-        } else {
-          return cycle
-        }
-      }),
-    )
+    dispatch({
+      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      payload: {
+        activeCycleId,
+      },
+    })
+
+    // setCycles((cycles) =>
+    //   cycles.map((cycle) => {
+    //     if (cycle.id === activeCycleId) {
+    //       return { ...cycle, finishedDate: new Date() }
+    //     } else {
+    //       return cycle
+    //     }
+    //   }),
+    // )
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -66,21 +87,36 @@ export function CyclesContextProvider({
       startDate: new Date(),
     }
 
-    setCycles((cycles) => [...cycles, newCycle])
+    // Enviaremos alguma informação que lá dentro do Reducer consigamos distinguir uma action da outra
+    dispatch({
+      type: 'ADD_NEW_CYCLE',
+      payload: {
+        newCycle,
+      },
+    })
+
+    // setCycles((cycles) => [...cycles, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    setCycles((cycles) =>
-      cycles.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date() }
-        } else {
-          return cycle
-        }
-      }),
-    )
+    dispatch({
+      type: 'INTERRUPT_CURRENT_CYCLE',
+      payload: {
+        activeCycleId,
+      },
+    })
+
+    // setCycles((cycles) =>
+    //   cycles.map((cycle) => {
+    //     if (cycle.id === activeCycleId) {
+    //       return { ...cycle, interruptedDate: new Date() }
+    //     } else {
+    //       return cycle
+    //     }
+    //   }),
+    // )
 
     setActiveCycleId(null)
   }
